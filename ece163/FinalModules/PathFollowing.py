1afc7b71f222bb5f5564d5d59f155b8f6c4c9c8b
Miguel Tamayo <miiguell098@MiggsXPS>
2023-03-13 15:57:52 -0700

    combine path following

---
+++ ece163/FinalModules/PathFollowing.py
@@ -0,0 +1,91 @@
+import ece163.Utilities.MatrixMath as mm
+import math
+import ece163.Containers.States as States
+
+
+def CalcPathCourseAngle(q: 'list[list[float]]'):
+    """
+    Calculate the unit vector normal to the q-k^i plane.
+    @param: q -> path direction of the unit vector k^i
+    """
+    return math.atan2(q[0][0], q[1][0])
+
+def CalcR_Inertial2Path(chi: float):
+    """
+    Calculate the rotation matrix from inertial to path
+    @param: chi -> path course angle
+    """
+    return [[math.cos(chi), math.sin(chi), 0], [-math.sin(chi), math.cos(chi), 0], [0,0,1]]
+
+def CalcRelativePathError(state: States.vehicleState, origin:'list[list[float]]' , R: 'list[list[float]]'):
+    """
+    Calculate the Relative Path Error
+    @param: state -> current state of UAV
+    @param: origin -> position of origin point that the line to waypoint is.
+    @param: R -> Rotation matrix inertial to path
+
+    """
+    position = [[state.pn], [state.pe], [state.pd]]
+    return mm.multiply(R, mm.subtract(position, origin))
+
+def CalcCommandedCourse(q: 'list[list[float]]', origin: 'list[list[float]]', chi_inf: float, k_path: float, state:States.vehicleState):
+    """
+    Calculate the Commanded Course, uses previous functions
+    @param: q -> path direction of the unit vector k^i
+    @param: origin -> position of origin point that the line to waypoint is.
+    @param: chi_inf -> gain that indicates what angle the UAV will approach waypoint from afar
+    @param: k_path -> gain that indicates how soon the UAV starts turning
+    @param: state -> current state of the UAV
+
+    """
+    chi_q = CalcPathCourseAngle(q)
+    R = CalcR_Inertial2Path(chi_q)
+    e = CalcRelativePathError(state=state, origin=origin, R=R)
+    return chi_q - chi_inf * (2 / math.pi) * math.atan(k_path * e)
+
+
+
+
+def CalcUnitNormalVector(q:'list[list[float]]'):
+    """
+    Author: Miguel Tamayo (miatamay)
+    Date: 03.13.2023
+    Calculate the unit vector normal to the q-k^i plane.
+    @param: q -> path direction of the unit vector k^i
+    """
+
+    ki = [[0], [0], [1]] # vertical unit vector
+    cross = mm.crossProduct(q, ki)
+    mag = math.hypot(ki[0][0], ki[1][0], ki[2][0])
+
+    return mm.scalarMultiply(1.0/mag, cross)
+
+def CalcProjectedRelativeErrorVector(state:States.vehicleState, origin, R, n:'list[list[float]]', r:'list[list[float]]'):
+    """
+    Author: Miguel Tamayo (miatamay)
+    Date: 03.13.2023
+    @param: state -> vehicle state
+    @param: origin -> vector representing path origin
+    @param: R -> rotation matrix from inertial to path frame
+    @param: n -> unit normal vector
+    @param: r -> vector distance from origin of path
+    """
+
+    # relative error vector in the inertial frame
+    epi = [[state.pn - r[0][0]], [state.pe - r[1][0]], [state.pd - r[2][0]]]
+    epi_dot_n = mm.dotProduct(epi, n) # epi.n
+    epi_n = mm.multiply(epi_dot_n, n) # (epi.n)n
+    
+    si = mm.subtract(epi, epi_n) # epi - (epi.n)n
+    return si
+
+def CalcCommandedHeight(s, r, q):
+    """
+    Author: Miguel Tamayo
+    Date: 03.13.2023
+    Calculate
+    """
+    
+    hd = -r[2][0] + math.hypot(s[0][0], s[1][0]) * (q[2][0] / math.hypot(q[0][0], q[1][0]))
+
+    return hd
